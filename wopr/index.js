2d463941abe006a301f7d2490cb9eb24653bbf80
M.S.T.O.P <mstop4@gmail.com>
2021-03-07 19:20:46 -0500

    compute tic tac toe results

--- wopr/index.js
+++ wopr/index.js
@@ -1,4 +1,14 @@
 let count = 0;
+const winRows = [
+  [ 0, 1, 2 ],
+  [ 3, 4, 5 ],
+  [ 6, 7, 8 ],
+  [ 0, 3, 6 ],
+  [ 1, 4, 7 ],
+  [ 2, 5, 8 ],
+  [ 0, 4, 8 ],
+  [ 2, 4, 6 ],
+]
 
 const swap = (array, indexA, indexB) => {
   const temp = array[indexA];
@@ -6,34 +16,75 @@ const swap = (array, indexA, indexB) => {
   array[indexB] = temp;
 }
 
-const permute = (array, indexA, indexB) => {
+const checkEqual = (a, b, c) => {
+  return a === b && a === c && a != null;
+}
+
+const permuteMoves = (movesList, indexA, indexB) => {
   // Base case 
   if (indexA === indexB) {
     count++;
-    console.log(count);
+    ticTacToe(movesList);
   } else {
     // Permutations made 
     for (let i = indexA; i <= indexB; i++) {
       // Swapping done 
-      swap(array, indexA, i);
+      swap(movesList, indexA, i);
 
       // Recursion called 
-      permute(array, indexA + 1, indexB);
+      permuteMoves(movesList, indexA + 1, indexB);
 
       // Backtrack 
-      swap(array, indexA, i);
+      swap(movesList, indexA, i);
     }
   }
 }
 
-const startPermutation = (arraySize) => {
-  const numArray = [];
+const ticTacToe = (movesList) => {
+  const grid = new Array(9);
+  let whosTurn = 'X';
+  let movesString = '';
 
-  for (let i = 0; i < arraySize; i++) {
-    numArray.push(i);
+  movesList.some((move, moveNum) => {
+    grid[move] = whosTurn;
+    movesString += move;
+    
+    if (moveNum+1 >= 5) {
+      const winner = checkWinner(grid);
+      if (winner !== 'NONE') {
+        console.log(movesString);
+        console.log(`WINNER: ${winner}`);
+        return true;
+      }
+    }
+
+    whosTurn = whosTurn === 'X' ? 'O' : 'X';
+    return false;
+  });
+
+  console.log(movesString);
+  console.log(`WINNER: NONE`);
+}
+
+const checkWinner = (grid) => {
+  for (const row of winRows) {
+    if (checkEqual(grid[row[0]], grid[row[1]], grid[row[2]])) {
+      return grid[row[0]];
+    }
   }
 
-  permute(numArray, 0, numArray.length - 1)
+  return 'NONE';
 };
 
-startPermutation(9);
+
+const startJoshua = (numMoves) => {
+  const movesList = [];
+
+  for (let i = 0; i < numMoves; i++) {
+    movesList.push(i);
+  }
+
+  permuteMoves(movesList, 0, movesList.length - 1)
+};
+
+startJoshua(9);
